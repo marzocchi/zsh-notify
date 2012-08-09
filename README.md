dc2858baa13597363ca5777e57507d43778e65b9
marzocchi <marzocchi@me.com>
2012-08-09 20:32:45 +0200

    import.

---
+++ README.md
@@ -0,0 +1,40 @@
+zsh-notify
+=======
+
+A plugin for the Z shell (and OS X) that posts desktop notifications
+when any command fail or when a long-running command finish without
+errors, if the terminal application is in the background.
+
+Requirements
+---
+
+- [terminal-notifier.app][terminal-notifier] is required for posting to
+  Mountain Lion's Notification Center
+
+- [growlnotify][growlnotify] is required for posting to Growl in older
+  versions of Mac OS X.
+
+- Either Terminal.app or [iTerm2][iterm2].
+
+Usage: 
+---
+
+Just source it.
+
+Configuration:
+---
+
+By default, a notification about a "long-running", successful, command is
+posted only if it took at least 30 seconds to complete. To change this
+timeout, set the `NOTIFY_COMMAND_COMPLETE_TIMEOUT` environment variable
+to a value (in seconds).
+
+Also, the plugin assumes that `terminal-notifier.app` is installed in
+`/Applications` and that `growlnotify` lives in `/usr/local/bin`. You can
+change these defaults by setting the `$SYS_NOTIFIER` and `$GROWL_NOTIFIER`
+environment variables.
+
+[growlnotify]: http://growl.info/extras.php/#growlnotify
+[terminal-notifier]: https://github.com/alloy/terminal-notifier 
+[iterm2]: http://www.iterm2.com/
+
---
+++ notify.plugin.zsh
@@ -0,0 +1,56 @@
+# vim: set nowrap filetype=zsh:
+# 
+# See README.md.
+#
+fpath=($fpath `dirname $0`)
+
+# Default timeout is 30 seconds.
+[[ $NOTIFY_COMMAND_COMPLETE_TIMEOUT == "" ]]  \
+  && NOTIFY_COMMAND_COMPLETE_TIMEOUT=30
+
+# Notify an error with no regard to the time elapsed (but always only
+# when the terminal is in background).
+function notify-error {
+  local icon
+  icon="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns"
+  notify-if-background -t "#fail" --image "$icon" < /dev/stdin &!
+}
+
+# Notify of successful command termination, but only if it took at least
+# 30 seconds (and if the terminal is in background).
+function notify-success() {
+  local now diff start_time last_command
+
+  start_time=$1
+  last_command="$2"
+  now=`date "+%s"`
+
+  ((diff = $now - $start_time ))
+  if (( $diff > $NOTIFY_COMMAND_COMPLETE_TIMEOUT )); then
+    notify-if-background -t "#win" <<< "$last_command" &!
+  fi
+}
+
+# Notify about the last command's success or failure.
+function notify-command-complete() {
+  last_status=$?
+  if [[ $last_status -gt "0" ]]; then
+    notify-error <<< $last_command
+  elif [[ -n $start_time ]]; then
+    notify-success "$start_time" "$last_command"
+  fi
+  unset last_command start_time last_status
+}
+
+function store-command-stats() {
+  last_command=$1
+  last_command_name=${1[(wr)^(*=*|sudo|ssh|-*)]}
+  start_time=`date "+%s"`
+}
+
+
+autoload -U tell-terminal
+autoload -U tell-iterm2
+autoload -U notify-if-background
+add-zsh-hook preexec store-command-stats
+add-zsh-hook precmd notify-command-complete
