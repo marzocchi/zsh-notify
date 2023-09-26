917dc001abbb7f447d95926ef8a49d0256095781
Chris Pick <chris@chrispick.com>
2023-09-26 11:19:03 -0400

    Add WezTerm support
    Depends on installing [`jq`](https://github.com/jqlang/jq),
    activating WezTerm's
    [shell integration](https://wezfurlong.org/wezterm/shell-integration.html?h=integration#user-vars)
    (for the `__wezterm_set_user_var()` shell function), and placing
    following snippet in WezTerm's
    [.wezterm.lua](https://wezfurlong.org/wezterm/config/files.html#quick-start):
    ```lua
    local wezterm = require 'wezterm'
    local config = {}
    
    wezterm.on('user-var-changed', function(window, pane, name, value)
      if name ~= "NOTIFICATION" then
        return
      end
    
      local notification = wezterm.json_parse(value)
    
      if notification.check_focus
        and window:is_focused()
        and window:active_pane():pane_id() == pane:pane_id()
      then
        return
      end
    
      if not notification.timeout_milliseconds then
        notification.timeout_milliseconds = nil
      end
    
      window:toast_notification(notification.title, notification.message,
        nil, notification.timeout_milliseconds)
    end)
    
    return config
    ```

--- notify.plugin.zsh
+++ notify.plugin.zsh
@@ -2,7 +2,9 @@
 
 plugin_dir="$(dirname $0:A)"
 
-if [[ "$TERM_PROGRAM" == 'iTerm.app' ]] || [[ "$TERM_PROGRAM" == 'Apple_Terminal' ]] || [[ -n "$ITERM_SESSION_ID" ]] || [[ -n "$TERM_SESSION_ID" ]]; then
+if [[ "$TERM_PROGRAM" == 'WezTerm' ]]; then
+    source "$plugin_dir"/wezterm/functions
+elif [[ "$TERM_PROGRAM" == 'iTerm.app' ]] || [[ "$TERM_PROGRAM" == 'Apple_Terminal' ]] || [[ -n "$ITERM_SESSION_ID" ]] || [[ -n "$TERM_SESSION_ID" ]]; then
     source "$plugin_dir"/applescript/functions
 elif [[ "$DISPLAY" != '' ]] && command -v xdotool > /dev/null 2>&1 &&  command -v wmctrl > /dev/null 2>&1; then
     source "$plugin_dir"/xdotool/functions
